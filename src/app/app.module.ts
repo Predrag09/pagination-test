5a7d88e5dd624d99aba0e1bacf9e84cbad4d32a5
Predrag09 <predrag.c@droxic.com>
2022-04-08 18:49:13 +0200

    Outsource items component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PaginationModule } from './pagination/pagination.module';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
